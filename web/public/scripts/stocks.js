923f2faaed570b5bcdf07bd74bd93bbb1436af53
Tomas Kupka <tomas@comoyo.com>
2013-10-27 01:33:03 +0200

    Added linear regression on stocks table

--- web/public/scripts/stocks.js
+++ web/public/scripts/stocks.js
@@ -1,7 +1,7 @@
 
 var app = angular.module("stocksApp", ["ngGrid"]);
 
-function MyCtrl($scope, $http) {
+function dayTopStocksCtrl($scope, $http) {
   $http({method: "GET", url: "/dayTopStocks"})
     .success(function(data, status, headers, config) {
       $scope.dayTopStocks = data;
@@ -20,3 +20,24 @@ function MyCtrl($scope, $http) {
   $scope.dayTopStocks = [];
 }
 
+function linearRegressionCtrl($scope, $http) {
+  $http({method: "GET", url: "/linearRegression"})
+    .success(function(data, status, headers, config) {
+      $scope.linearRegression = data;
+  })
+    .error(function(data, status, headers, config) {
+      console.log("Error getting linear regression on stocks");
+  });
+
+  $scope.gridOptions = {
+    data: 'linearRegression',
+    showFilter: true,
+    columnDefs: [{ field: "name", width: 120, displayName: "Name"},
+                { field: "gradient", width: 120, displayName: "Gradient"},
+                { field: "intercept", width: 120, displayName: "Intercept"},
+                { field: "r_value", width: 120, displayName: "r_value"},
+                { field: "p_value", width: 120, displayName: "p_value"},
+                { field: "std_err", width: 120, displayName: "std_err"}]
+  };
+  $scope.linearRegression = [];
+}
--- web/server.js
+++ web/server.js
@@ -3,7 +3,7 @@ var app = express()
 
 var sqlite3 = require("sqlite3").verbose();
 
-app.get("/dayTopStocks", function(req, res){
+app.get("/dayTopStocks", function(req, res) {
   var db = new sqlite3.Database("stocks.db", sqlite3.OPEN_READONLY);
   sqlite3.verbose();
 
@@ -20,6 +20,34 @@ app.get("/dayTopStocks", function(req, res){
           datapoint.name = rows[i].name;
           datapoint.datum = rows[i].datum;
           datapoint.gain = rows[i].gain;
+          data.push(datapoint);
+        }
+      console.log(data);
+      res.send(data);
+      db.close();
+    });
+  });
+});
+
+app.get("/linearRegression", function(req, res) {
+  var db = new sqlite3.Database("stocks.db", sqlite3.OPEN_READONLY);
+  sqlite3.verbose();
+
+  db.serialize(function() {
+    var data = [];
+    db.all("select t.name as name, gradient, intercept, r_value, p_value, std_err " +
+          "from linear_regression l, tickers t " +
+          "where l.ticker = t.id " +
+          "order by p_value, gradient desc",
+      function(err, rows) {
+        for (i = 0; i < rows.length; i++) {
+          datapoint = {};
+          datapoint.name = rows[i].name;
+          datapoint.gradient = rows[i].gradient;
+          datapoint.intercept = rows[i].intercept;
+          datapoint.r_value = rows[i].r_value;
+          datapoint.p_value = rows[i].p_value;
+          datapoint.std_err = rows[i].std_err;
           data.push(datapoint);
         }
       console.log(data);
